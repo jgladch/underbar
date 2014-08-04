948c4aee171da7bf3a167ae64ed9191bfd1cdf1e
jgladch <jeffgladchun@gmail.com>
2014-08-03 19:50:12 -0700

    2/3rds through contain

--- src/underbar.js
+++ src/underbar.js
@@ -169,6 +169,19 @@ var _ = {};
   //     return total + number;
   //   }, 0); // should be 6
   _.reduce = function(collection, iterator, accumulator) {
+    var result;
+    if (accumulator !== undefined) {
+      result = accumulator;
+    } else {
+      result = collection[0];
+    };
+
+    for (var i = 0; i < collection.length; i++) {
+      result = iterator(result, collection[i]);
+    };
+
+    return result;
+
   };
 
   // Determine if the array or object contains a given value (using `===`).
