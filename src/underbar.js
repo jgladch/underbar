ee1f3b3e473a0baf5012d33c0d4083740a2cf68f
Jeff Gladchun <jeffgladchun@gmail.com>
2014-08-12 14:11:08 -0700

    sortBy is now working

--- src/underbar.js
+++ src/underbar.js
@@ -400,6 +400,25 @@ var _ = {};
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    return collection.sort(function(a, b){
+      if (typeof iterator === 'string') {
+        if (a[iterator] < b[iterator]) {
+          return -1;
+        } else if (a[iterator] > b[iterator]){
+          return 1;
+        } else {
+          return 0;
+        };
+      } else {
+        if (iterator(a) < iterator(b)) {
+          return -1;
+        } else if (iterator(a) > iterator(b)){
+          return 1;
+        } else {
+          return 0;
+        };
+      };
+    });
   };
 
   // Zip together two or more arrays with elements of the same index
